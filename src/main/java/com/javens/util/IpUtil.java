38bb8eba250ead4934059258387d4091a17ba7ae
liujing <82839618@qq.com>
2018-02-01 11:10:21 +0800

    README

--- src/main/java/com/javens/util/IpUtil.java
+++ src/main/java/com/javens/util/IpUtil.java
@@ -1,5 +1,6 @@
 package com.javens.util;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 
 
@@ -10,6 +11,20 @@ public class IpUtil {
         } catch (Exception e) {
             return "";
         }
+    }
+
+    public String getRemoteHost(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getRemoteAddr();
+        }
+        return ip.equals("0:0:0:0:0:0:0:1")?"127.0.0.1":ip;
     }
 
 }
